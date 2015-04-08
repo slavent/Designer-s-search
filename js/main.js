64688295d04339805f75ac0a93019a6075a31ef8
Slavent <startupne@gmail.com>
2015-04-08 20:03:44 +0300

    init

--- js/main.js
+++ js/main.js
@@ -2,9 +2,34 @@
 
 	window.App = {};
 	window.App.init = function(){
-		console.log("App init");
+		
+			/* Bind social-share */
+            $(".social-share a").on("click", function () {
+                var $this = $(this);
+
+                $(".social-likes").find(".social-likes__widget_" + $this.data("social")).click();
+                return false;
+            });
+
+            /* Hide social-share vertical on scroll to footer */
+            var doc_h = $(document).height(),
+            	window_h = $(window).height(),
+            	box_h = $(".js-hide-social-share").height();
+
+            if($(document).scrollTop() < doc_h - (window_h + box_h)) {
+            	$(".social-share_pos_vertical").show();
+            }
+
+            $(document).scroll(function(){
+            	if($(document).scrollTop() > doc_h - (window_h + box_h)) {
+            		$(".social-share_pos_vertical").fadeOut();
+            	} else {
+            		$(".social-share_pos_vertical").fadeIn();
+            	}
+            });      
+
 	};
 
 	window.App.init();
 
-}(jQuery, window));
+}(jQuery, this));
